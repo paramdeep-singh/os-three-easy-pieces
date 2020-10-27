8b1d14b89a31976f22b85f6ed84c0ba796641878
hippie <paramdeep@live.com>
2020-10-27 11:19:56 -0500

    Refactored fork return variable name, created skeleton for the second question

--- cpu-api/2.c
+++ cpu-api/2.c
@@ -3,21 +3,16 @@
 #include <stdlib.h>
 
 int main() {
-    int x = 100;
-    int rc = fork();
+    int c_pid = fork();
 
-    if (rc < 0) {
+    if (c_pid < 0) {
         fprintf(stderr, "fork failed\n");
         exit(EXIT_FAILURE);
     }
-    if (rc == 0) {
+    if (c_pid == 0) {
         // child process
-        x = 101;
-        printf("CHILD: x = %d\n", x);
     } else {
         // parent process
-        x = 102;
-        printf("PARENT: x = %d\n", x);
     }
     return 0;
 }
